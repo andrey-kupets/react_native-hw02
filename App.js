2a65757ee330ddf7d469b89d062ff027a1da3133
andrey-kupets <andrey.kupets@i.ua>
2021-04-29 00:07:19 +0300

    axios

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {getUsers} from "./src/api/API";
 
 export default function App() {
+
   return (
     <View>
 
